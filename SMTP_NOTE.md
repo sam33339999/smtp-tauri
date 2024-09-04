8108226232b53a6ed724816a501020ca8e73a399
sam.zheng <sam33339999@gmail.com>
2024-09-04 22:36:59 +0800

    non-blocking with send email

---
+++ SMTP_NOTE.md
@@ -0,0 +1,13 @@
+# SMTP (Simple Mail Transfer Protocol)
+> 一種用於發送和接收電子郵件的網路標準協議；<br/>
+> 他定義了電子郵件伺服器之間如何互相傳送、接收與中繼郵件的規則。<br/>
+> 郵件的接收由 `POP3 (郵局協議|接收)` 或 `IMAP (網路消息訪問協議|發送)` 來處理。
+
+- POP3 (Post Office Protocol 3)
+> POP3 是 TCP/IP 協議中的一員，由 RFC1939 進行定義。<br/>
+> 本協議主要用於支持使用客戶端遠端管理在服務器上的電子郵件。<br/>
+> 提供了 SSL 加密的 POP3 協議，即 `POP3S`。
+- IMAP (Internet Message Access Protocol)
+> IMAP 以前稱作`交互郵件訪問協議`是一個應用層協議，
+> 用來以本地郵件客戶端工具(如: MS Outlook, Outlook Express, Foxmail, Mozilla Thunderbird) 存取遠端服務器的郵件。<br/>
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use lettre::message::header::ContentType;
 // use lettre::{SmtpTransport, transport::smtp::{authentication::Credentials}, Message};
 use lettre::{Message, SmtpTransport, Transport};
 use lettre::transport::smtp::authentication::Credentials;
 use serde::{Deserialize, Serialize};
+use tokio::task;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 struct SmtpConfig {
@@ -30,36 +32,34 @@ struct SmtpCombine {
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn send_mail(smtp: SmtpCombine) -> Result<String, String> {
-    println!("Received form data: {:?}", smtp);
+async fn send_mail(smtp: SmtpCombine) -> Result<String, String> {
+    task::spawn_blocking(move || {
+        let credentials = Credentials::new(
+            smtp.config.username.clone(),
+            smtp.config.password.clone(),
+        );
 
-    let credentials = Credentials::new(
-        smtp.config.username.clone(),
-        smtp.config.password.clone(),
-    );
-
-    let mail = Message::builder()
+        let mail = Message::builder()
         .from(smtp.content.from.clone().parse().unwrap())
         .to(smtp.content.to.clone().parse().unwrap())
         .subject(smtp.content.subject.clone())
+        .header(ContentType::TEXT_HTML)
         .body(smtp.content.html.clone())
         .unwrap();
 
-    
-    let mailer = SmtpTransport::relay(smtp.config.host.clone().as_str())
-        .unwrap()
-        .port(smtp.config.port.clone())
-        .credentials(credentials)
-        .build();
+        // let mailer = SmtpTransport::builder_dangerous(smtp.config.host.clone().as_str())
+        let mailer = SmtpTransport::relay(smtp.config.host.clone().as_str())
+            .unwrap()
+            .port(smtp.config.port.clone())
+            .credentials(credentials)
+            .build();
 
-
-    match mailer.send(&mail) {
-        Ok(_) => Ok(format!("Email sent successfully!")),
-        Err(e) => Err(format!("Could not send email: {:?}", e)),
-    }
+        match mailer.send(&mail) {
+            Ok(_) => Ok(format!("Email sent successfully!")),
+            Err(e) => Err(format!("Could not send email: {:?}", e)),
+        }
+    }).await.unwrap()
 }
-
-
 
 fn main() {
     tauri::Builder::default()
